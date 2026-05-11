0219b242f3feab0fc38a8a4b646831bf182cac28
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-05-11 15:51:46 +0200

    Add controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,62 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const [name, setName] = React.useState("")
+    const [age, setAge] = React.useState(0)
+    const [comment, setComment] = React.useState("")
+    const [signup, setSignup] = React.useState(false)
+
     return (
         <div>
+            <form>
+                <legend>
+                    Gegevens
+                    <label>
+                        Naam:
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
 
+                    <label>
+                        Leeftijd:
+                        <input
+                            type="number"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                    </label>
+                </legend>
+
+                <legend>
+                    Jouw review
+
+                    <label>
+                        Opmerkingen:
+                        <textarea
+                            placeholder={"Wat vond je van het recept?"}
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                        >
+                    </textarea>
+                    </label>
+
+                    <label>
+                        <input
+                            type="checkbox"
+                            value={signup}
+                            onChange={(e) => setSignup(e.target.checked)}
+                        />
+                        Ik schrijf me in voor de nieuwsbrief
+                    </label>
+                    <button type="submit">
+                        Versturen
+                    </button>
+                </legend>
+            </form>
         </div>
     );
 }
---
+++ src/Notes.md
@@ -0,0 +1,4 @@
+1. **Waarom wordt state ook toegepast op `form`?**
+   - `form` is onderdeel van de UI, en alles wat zichtbaar is en kan veranderen, wordt in React beheerd via state.
+   - Je kunt anders de data in `form` niet gebruiken voor validatie, weergave of het versturen van gegevens.
+2.
